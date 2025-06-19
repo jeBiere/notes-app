497f87aa7f89b41b5f7607f2758047a036d93b25
je_biere <lenkis.fedor@gmail.com>
2025-06-19 20:30:25 +0300

    Added some users routers

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -2,35 +2,27 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from src.schemas.user_schemas import UserCreate
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db import get_async_db
-from src.crud.user_crud import get_user_by_email, get_users
-from src.crud.auth_crud import register_user
+from src.crud.user_crud import get_users, get_user_by_email, get_user_by_id
+
+
 router = APIRouter(
     prefix="/user",
     tags=["user"],
 )
 
-
-@router.post("/registration")
-async def registration(user_in: UserCreate, db: AsyncSession = Depends(get_async_db)):
-    existing = await get_user_by_email(db, user_in.email)
-    if existing:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
-            detail="Email уже зарегистрирован",
-        )
-    try:
-        user = await register_user(db, user_in)
-    except ValueError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=(str(e))
-        )
-    return user
-
-
 @router.get("/users")
 async def read_users(db: AsyncSession = Depends(get_async_db)):
     result = await get_users(db)
+    return result
+    
+@router.get("/users/{user_id}")
+async def read_user_by_id(user_id: str, db: AsyncSession = Depends(get_async_db)):
+    result = await get_user_by_id(db, user_id=user_id)
+    return result
+    
+@router.get("/users/{user_emal}")
+async def read_user_by_email(user_email: str, db: AsyncSession = Depends(get_async_db)):
+    result = await get_user_by_email(db, email=user_email)
     return result
     
     
